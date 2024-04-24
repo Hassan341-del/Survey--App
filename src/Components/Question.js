65bbb7fe97ca7587982a6b507f7c6c29bd0cac11
Hassan Ali <hassanalibhatti648@gmail.com>
2024-04-24 16:35:39 +0500

    Fix Bugs

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,29 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Question.css'
 export default function Question(props) {
+  const [showOtherInput, setShowOtherInput] = useState(false)
+  const handleOtherInput = (e) => {
+    setShowOtherInput(e.target.value === "Other" && e.target.checked)
+  }
   return (
     <>
     <div className="questions">
     <div className="fields">
       <form onSubmit={props.submit}>
       <p><b>3.</b> What are you currently doing</p>
-      <div className='form-check form-check-inline'>
-        <input className="form-check-input" type="radio" name="Profession" id="inlineRadio1" value="Teaching"/>
+      <div className='form-check form-check-inline' style={{marginBottom: "1rem"}}>
+        <input className="form-check-input" type="radio" name="Profession" id="inlineRadio1" value="Teaching" onChange={handleOtherInput}/>
         <label className="form-check-label" htmlFor="inlineRadio1">Teaching</label>
       </div>
       <div className='form-check form-check-inline'>
-        <input className="form-check-input" type="radio" name="Profession" id="inlineRadio2" value="Student"/>
+        <input className="form-check-input" type="radio" name="Profession" id="inlineRadio2" value="Student" onChange={handleOtherInput}/>
         <label className="form-check-label" htmlFor="inlineRadio2">Student</label>
       </div>
       <div className='form-check form-check-inline'>
-        <input className="form-check-input" type="radio" name="Profession" id="inlineRadio3" value="Programmer"/>
+        <input className="form-check-input" type="radio" name="Profession" id="inlineRadio3" value="Programmer" onChange={handleOtherInput}/>
         <label className="form-check-label" htmlFor="inlineRadio3">Programmer</label>
       </div>
       <div className='form-check form-check-inline'>
-        <input className="form-check-input" type="radio" name="Profession" id="inlineRadio4" value="Other"/>
+        <input className="form-check-input" type="radio" name="Profession" id="inlineRadio4" value="Other" onChange={handleOtherInput}/>
         <label className="form-check-label" htmlFor="inlineRadio4">Other</label>
       </div>
-        <input type="text" className='form-control not-listed-input' placeholder='Type here if not listed' name='other'/>
+      {
+        showOtherInput ? (
+          <input type="text" className='form-control not-listed-input' placeholder='Type here if not listed' name='other'/>
+        ):null
+      }
         <p><b>4.</b> Please rate our course</p>
         <div className='form-check form-check-inline'>
         <input className="form-check-input" type="radio" name="Rating" id="inlineRadio5" value="Poor" />
@@ -46,3 +54,7 @@ export default function Question(props) {
     </>
   )
 }
+
+
+
+
